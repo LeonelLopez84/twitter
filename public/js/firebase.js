28bf1ca8d996e58982de93dd6f5281db58160541
Leonel López <leonel@blako.com>
2017-12-19 13:31:10 -0600

    instalación

---
+++ public/js/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBj-KRJ6f_lihfBjg4-ItcJXp3ZQnbOvMU",
+    authDomain: "blako-b4531.firebaseapp.com",
+    databaseURL: "https://blako-b4531.firebaseio.com",
+    projectId: "blako-b4531",
+    storageBucket: "blako-b4531.appspot.com",
+    messagingSenderId: "1024985583286"
+  };
+  firebase.initializeApp(config);
